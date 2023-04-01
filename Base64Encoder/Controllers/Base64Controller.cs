14e6ab26f3e7feb04629116ab37fb59524246891
scarfreak <senjohnholly22@gmail.com>
2023-04-01 20:33:12 +0800

    Add files via upload

---
+++ Base64Encoder/Controllers/Base64Controller.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace Base64Encoder.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Base64Controller : ControllerBase
+    {
+        [HttpPost("encode")]
+        public IActionResult Encode(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return BadRequest("Text cannot be empty");
+            }
+
+            Random random = new Random();
+            int delay = random.Next(1000, 5000); // Generate a random delay between 1 to 5 seconds
+            System.Threading.Thread.Sleep(delay); // Wait for the random delay
+            var encodedText = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(text));
+            return Ok(encodedText);
+        }
+    }
+}
